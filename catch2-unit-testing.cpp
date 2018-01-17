9608a62c13033f4243ada6092a25690bd88842fb
tonyoreglia <ajoreglia@gmail.com>
2018-01-18 00:06:59 +1100

    added starting position testing for white

--- catch2-unit-testing.cpp
+++ catch2-unit-testing.cpp
@@ -16,4 +16,11 @@ TEST_CASE( "Starting position", "[position]" ) {
     REQUIRE( position.GetRooksBitBoard(BLACK) == (1ULL | 1ULL << 7) );
     REQUIRE( position.GetPawnsBitBoard(BLACK) == 0xFF00 );
 
+    REQUIRE( position.GetKingBitBoard(WHITE) == 1ULL << 60 );
+    REQUIRE( position.GetQueenBitBoard(WHITE) == 1ULL << 59 );
+    REQUIRE( position.GetKnightsBitBoard(WHITE) == (1ULL << 57 | 1ULL << 62) );
+    REQUIRE( position.GetBishopsBitBoard(WHITE) == (1ULL << 58 | 1ULL << 61) );
+    REQUIRE( position.GetRooksBitBoard(WHITE) == (1ULL << 56 | 1ULL << 63) );
+    REQUIRE( position.GetPawnsBitBoard(WHITE) == 0xFF000000000000 );
+
 }
